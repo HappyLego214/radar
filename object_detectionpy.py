6a96173d0c44692603bb4105fe3818b808acd43b
josh <anthony21josh@gmail.com>
2024-10-22 00:38:16 +0800

    adds sectoring detection

--- object_detectionpy.py
+++ object_detectionpy.py
@@ -3,33 +3,45 @@ import matplotlib.pyplot as plt
 
 radar = {'position': (0,0), 'range': 100}
 
-objects_in_area = [(50,30), (5,10), (120,50), (10,500)]
-sectors = {'top-left': [(-100, 0), (0,100)], 
-           'top-right': [(0, 100), (100, 0)],
-           'bottom-left': [(-100, 0), (0, -100)],
-           'bottom-right': [(0, -100), (100, 0)]}        
+objects_in_area = [(-50,10), (50,30), (80,-50), (-10,-80)]
+sector_bounderies = {
+    'top-left': [(-100, 0), (0,100)], 
+    'bottom-left': [(-100, 0), (0, -100)],
+    'top-right': [(0, 100), (100, 0)],
+    'bottom-right': [(0, -100), (100, 0)],
+    }        
 
 def calcDistance(radar, object):
     distance = math.sqrt(math.pow((radar[0] - object[0]), 2) + math.pow((radar[1] - object[1]), 2))
     return distance
 
-def scanRadar(sectors, objects_in_area, radar):
-    detected_objects = []
-    for object in objects_in_area:
-        distance = calcDistance(radar['position'], object)
-        if distance <= radar['range']:
-            detected_objects.append(object)
-    
-    return detected_objects
+def scanQuadrant(sector, object):
+    x1, y1 = sector[0]
+    x2, y2 = sector[1]
+    if min(x1, x2) <= object[0] <= max(x1, x2) and min(y1, y2) <= object[1] <= max(y1, y2):
+        return True
+    return False
 
-detected_objects = scanRadar(sectors, objects_in_area, radar)
+def scanRadar(sectors, objects_in_area, radar):
+    sector_objects = {
+    'top-left': [], 
+    'bottom-left': [],
+    'top-right': [],
+    'bottom-right': [],
+    }
+    
+    for idx, sector in sectors.items():
+        for obj in objects_in_area: 
+            obj_in_quad = scanQuadrant(sector, obj)
+            if obj_in_quad == True:    
+                distance = calcDistance(radar['position'], obj)
+                if distance <= radar['range']:
+                    sector_objects[idx].append(obj)
+            continue
+    
+    return sector_objects
+
+detected_objects = scanRadar(sector_bounderies, objects_in_area, radar)
         
 print(f"Number of Detected Objects: {len(detected_objects)} | Detected Objects: {detected_objects}")
         
-plt.figure()
-plt.scatter(*radar['position'], color='red', label='radar')
-plt.scatter(*zip(detected_objects), color='green', label='detected objects')
-plt.xlim(-radar['range'], radar['range'])
-plt.ylim(-radar['range'], radar['range'])
-plt.legend()
-plt.show()
